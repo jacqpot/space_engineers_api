a2cdc06c729c7d70a276799c04b2d25b282b7490
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-11-28 14:35:57 -0700

    update blocks controller and associasions for block and component

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,2 +1,4 @@
 class Block < ApplicationRecord
+    has_many :block_components
+    has_many :components, through: :block_components
 end
--- app/models/component.rb
+++ app/models/component.rb
@@ -1,2 +1,4 @@
 class Component < ApplicationRecord
+    has_many :block_components
+    has_many :blocks, through: :block_components
 end
